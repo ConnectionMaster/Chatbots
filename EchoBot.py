33355f4e405286927d15778f1bc131125f5f3bd6
BabakBar <babak.barghi@gmail.com>
2022-01-27 03:02:37 +0100

    fist

--- EchoBot.py
+++ EchoBot.py
@@ -2,11 +2,11 @@ bot_template = "BOT : {0}"
 user_template = "USER : {0}"
 
 # Define a function that responds to a user's message: respond
-def ____(____):
+def respond(message):
     # Concatenate the user's message to the end of a standard bot respone
-    bot_message = "____" + ____
+    bot_message = "I can hear you! You said:" + message
     # Return the result
-    return ____
+    return bot_message
 
 # Test function
 print(respond("hello!"))
